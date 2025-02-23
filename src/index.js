cfb4b80a5c44793afe32427029ed4cf669a66be7
Daniel Thompson <danl_thompson@hotmail.com>
2025-02-23 17:25:20 -0500

    fixed that dropped psace

--- src/index.js
+++ src/index.js
@@ -1,69 +1,46 @@
 const axios = require('axios')
 const parse = require('node-html-parser').parse;
 const {getGMail} = require('uuseCommons');
-const {
-  date,
-  capture,
-  doNotUpdate} = require('uuseCommons');
-const {extractFutureDate} = require('uuseCommons')
+const {date, capture, doNotUpdate} = require('uuseCommons');
 const {findStartingContent} = require('uuseCommons');
 const {getArticlesFromHTML,getAllArticles} = require('uuseCommons')
-const {cleanTitle, normalizeTitle} = require('uuseCommons')
-const {updateMenu,bulkInsert,bulkUpdate,
-       bulkDelete,fetchRecords, getEvents, 
-       getAxiosTemplate,
-       getRepeaters} = require('uuseCommons');
-const {append1, append3, divider} = require('uuseCommons')
-const {richContentToText,
-  getGeneratedDescriptionFromArticle,
-  getLongDescriptionFromArticle,
-  getTextFromArticle,
-  newEvent} = require('uuseCommons');
+const {getAxiosTemplate} = require('uuseCommons');
 
 const emmyS_BlogPost_ID ='f0c96956-2277-4491-a644-027e1b07bbd4';
 const my_BlogPost_ID    ='ec852cd6-a03d-4eab-9638-538fc58f2570';
+
 const the_BlogPost_ID = (doNotUpdate)?my_BlogPost_ID:emmyS_BlogPost_ID;
 console.log("DO NOT UPDATE",doNotUpdate,the_BlogPost_ID)
 
-const timestamp = new Date().toISOString();    // used in Rich Content annotations
-
 const filters = {subject: "Update From Emmy", from:"dcym@uuse.org"};
+const headers = getAxiosTemplate("").headers;
 
 getGMail(filters ,date, capture, (html)=>{
   const root = parse(html);
   const path = findStartingContent(root);
   const articles = getArticlesFromHTML(path);
-  const options = getAxiosTemplate("?");
-  options.url= '/posts/'+the_BlogPost_ID+"?fieldsets=RICH_CONTENT";
-  options.method = 'get';
-  options.baseURL= 'https://www.wixapis.com/blog/v3';
-  delete options.data
+  const article = articles[0][0];
 
-  let article = articles[0][1];
-  // article.shift();
-  // article = article.slice(0,-15);
-  const richContent = {nodes:article,documentStyle:{}};
-  const url = options.baseURL+'/draft-posts/'+the_BlogPost_ID
-  const updatedContent = {draftPost:{richContent}};
+  const url = `https://www.wixapis.com/blog/v3/draft-posts/${the_BlogPost_ID}?fieldsets=RICH_CONTENT`;
 
-  axios.patch(url, updatedContent, {
-      headers : options.headers
-    })
+  axios.patch(url, 
+        {draftPost:{
+         richContent:{nodes:article,documentStyle:{}},
+         firstPublishedDate:(new Date().toISOString())
+        }}, 
+        {headers})
       .then(response => {
-        console.log("Updated Blog Post:", response.status,response.data);
           const url = `https://www.wixapis.com/blog/v3/draft-posts/${the_BlogPost_ID}/publish`;
           if (!doNotUpdate)
           axios.post(url, {}, {
-            headers: options.headers
+            headers
           }).then(response =>{
-            console.log("Response:", response.data);
+            console.log("Emmy Blog Update Response:", response.data);
           })
       })
       .catch(error => {
-        console.error(url)
-        console.error(options.headers)
-        console.error(updatedContent)
         console.error("Error updating blog post:", error.response ? error.response.data : error);
+        console.error(`"_id ="${the_BlogPost_ID}`);
       });
   })
 //})
